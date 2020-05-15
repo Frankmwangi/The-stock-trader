67d6559fd8935637b53a01c03f561a94d0c2c4ee
Frank Mwangi <frankmwangi007@gmail.com>
2020-05-15 20:34:44 +0300

    passing of stocks to other child component

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>The Stocks component</h1>
+   <div>
+       <app-stock v-for="stock in stocks"></app-stock>
+   </div>
 </template>
+    <script>
+            import Stock from './Stock';
+            export default{
+                data() {
+                    return{
+                        stocks : [
+                        { id: 1, name: 'BMW', price:110},
+                         { id: 2, name: 'Google', price:200},
+                          { id: 3, name: 'Apple', price: 250},
+                           { id: 4, name: 'BMW', price: 8},
+                        ]
+                    }
+                },
+                components:{
+                    appStock: Stock
+                }
+
+            }
+    
+    </script>
